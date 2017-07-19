978cb552ca478e0f448377f09b14cfb12fc9b3f7
petitsurume <git@surume.tk>
2017-07-19 17:11:55 +0900

    hello world

--- src/launcher.js
+++ src/launcher.js
@@ -1,5 +1,5 @@
 const cluster = require("cluster")
-const cpuLength = process.env.CLUSTER_LIMIT || require("os").cpus.length
+const cpuLength = process.env.CLUSTER_LIMIT || require("os").cpus().length
 
 if (cluster.isMaster) {
     for (var i = 0; i < cpuLength ; i++) {
