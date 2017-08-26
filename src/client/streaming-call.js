a70ec40436a24b95b1058a4d28e648fa8c5a3bac
petitsurume <git@surume.tk>
2017-08-27 02:53:55 +0900

    ping(pongするとは言ってない)
    close #5

--- src/client/streaming-call.js
+++ src/client/streaming-call.js
@@ -1,3 +1,10 @@
 module.exports = function(endpoint){
-    return new WebSocket(location.origin.replace("http", "ws")+"/_/api/ws/"+endpoint+"?csrf="+(document.querySelector("meta[name=csrf-token]").content))
+    var ws = new WebSocket(location.origin.replace("http", "ws")+"/_/api/ws/"+endpoint+"?csrf="+(document.querySelector("meta[name=csrf-token]").content))
+    var interval = setInterval(function(){
+        ws.send("ping")
+    }, 10 * 1000)
+    ws.addEventListener("close", function() {
+        clearInterval(interval)
+    })
+    return ws
 }
